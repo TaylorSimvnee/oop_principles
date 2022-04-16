2a9fa9abea39a98e06f1aa3c88b58285e551c649
TaylorSimvnee <Mstaylorg98@gmail.com>
2022-04-16 10:01:24 -0700

    added company

---
+++ src/encapsulation/Company.java
@@ -0,0 +1,48 @@
+package encapsulation;
+
+public class Company {
+
+
+    //Create instance variables name, address, phone and encapsulation them
+
+    private String name;
+    private String address;
+    private String phone;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    //override ToSting method
+
+    // The  @Override annotation is optional
+    //now will print object attributes not location in memory
+    @Override
+    public String toString() {
+        return "Company{" +
+                "name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", phone='" + phone + '\'' +
+                '}';
+    }
+}
