47b2c4d48ab31518a88a9459322ee1f6c7efeae6
TaylorSimvnee <Mstaylorg98@gmail.com>
2022-04-16 13:41:19 -0700

    method overriding

--- src/inheritance/Tester.java
+++ src/inheritance/Tester.java
@@ -33,3 +33,17 @@ public class Tester extends Person{
         System.out.println("Tester walks");
     }
 }
+/*
+Overriding =
+    -Method overriding is only in a child class -- inheritance is there
+    -Your have a method in the parent class that you are not satisfied
+      with its implementation (body), and you would like to change it.
+    - The method name , return type and arguments must be the same
+    -Body should be different , otherwise there is no point in overriding
+    - THE PURPOSE OF OVERRIDING A METHOD : overriding a parent class' method in a child class
+
+
+    METHOD OVERLOADING
+    -In the same class and has nothing to do with inheritance
+    - multiple methods in the same class with the same name but changing either number or type of parameters
+ */
